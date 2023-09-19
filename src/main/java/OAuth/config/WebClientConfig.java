ec29b9985ef7c0415107b00eb7759d2c3ced1be1
dgjinsu <rlawlstn3206@naver.com>
2023-09-19 14:27:35 +0900

    feat: webClientConfig 파일 작성

---
+++ src/main/java/OAuth/config/WebClientConfig.java
@@ -0,0 +1,12 @@
+package OAuth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder().build();
+    }
+}
